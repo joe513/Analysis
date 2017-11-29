b23a97b560f127c3b7339775c762244847a82cb3
Jabrail <lezgintsev13@yandex.ru>
2017-11-29 22:17:26 +0300

    Added some files

--- ObjectAnalysis.py
+++ ObjectAnalysis.py
@@ -113,5 +113,5 @@ def the_most_important_info_of_object(obj):
 
 
 if __name__ == '__main__':
-	print('testing')
+    print('testing')
     the_most_important_info_of_object(open('five.txt', 'a+'))
